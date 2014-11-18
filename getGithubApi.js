a893edb1d80c7634dfc9fe0ee613723c042c32d9
yang-wei <yangweilim21@gmail.com>
2014-11-18 23:19:53 +0900

    filter JSON before insert into DB

--- getGithubApi.js
+++ getGithubApi.js
@@ -1,18 +1,34 @@
-var request = require('request');
+var request = require('request'),
+    filterJSON = require('./filterJSON');
+
 var GH_API_ROOT = 'https://api.github.com/';
 
 var options = {
-  url: GH_API_ROOT + 'repos/yang-wei/simplified',
+  url: GH_API_ROOT + 'search/repositories?q=language:javascript&per_page=3', 
   headers: {
     'User-Agent': 'request',
     'Content-type': 'application/json'
   }
 };
 
+var pickJSON = {
+  id: 'id',
+  name: 'name',
+  full_name: 'full_name',
+  owner: 'owner.login',
+  avatar_url: 'owner.avvatar_url',
+  url: 'html_url',
+  stars: 'stargazers_count',
+  watchers: 'watchers_count',
+  forks: 'forks',
+  language: 'language'
+};
+
 function fetchAPI(error, response, body) {
+  if(error) { console.log(response) }
   if(!error && response.statusCode === 200) {
     var result = JSON.parse(body);
-    console.log(body);
+    var storeDB = filterJSON(result.items, pickJSON);
   }
 }
 
