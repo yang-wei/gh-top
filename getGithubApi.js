8c3a8d99d1ca669ffe219050e41fdd87824b1fa3
yang-wei <yangweilim21@gmail.com>
2014-11-21 22:44:27 +0900

    use async pattern in fetchAPI method

--- getGithubApi.js
+++ getGithubApi.js
@@ -1,40 +1,43 @@
 var request = require('request'),
-    filterJSON = require('./filterJSON'),
-    updateDb = require('./updateDb');
+filterJSON = require('./filterJSON'),
+updateDb = require('./updateDb');
 
-var GH_API_ROOT = 'https://api.github.com/';
-
-var options = {
-  url: GH_API_ROOT + 'search/repositories?q=language:javascript&per_page=3', 
-  headers: {
-    'User-Agent': 'request',
-    'Content-type': 'application/json'
-  }
-};
-
-var pickJSON = {
-  gh_id: 'id',
-  name: 'name',
-  full_name: 'full_name',
-  owner: 'owner.login',
-  avatar_url: 'owner.avvatar_url',
-  url: 'html_url',
-  stars: 'stargazers_count',
-  watchers: 'watchers_count',
-  forks: 'forks',
-  language: 'language'
-};
-
-function fetchAPI(error, response, body) {
-  if(error) { console.log(response) }
-  if(!error && response.statusCode === 200) {
-    var result = JSON.parse(body);
-    var items = filterJSON(result.items, pickJSON);
-    updateDb(items, console.log);
-  }
+function LatestAPI(url) {
+   this.GH_API_ROOT = url;
+   this.options = {
+    url: this.GH_API_ROOT + 'search/repositories?q=language:javascript&per_page=5', 
+    headers: {
+      'User-Agent': 'nodejs',
+      'Content-type': 'application/json',
+      'Accept': 'application/vnd.github.v3+json'
+    }
+  };
+  
 }
 
-var requestApi = setInterval( function() { request(options, fetchAPI); }, 5000);
+LatestAPI.prototype.fetchAPI = function(callback) {
+    var pickJSON = {
+      gh_id: 'id',
+      name: 'name',
+      full_name: 'full_name',
+      owner: 'owner.login',
+      avatar_url: 'owner.avvatar_url',
+      url: 'html_url',
+      stars: 'stargazers_count',
+      watchers: 'watchers_count',
+      forks: 'forks',
+      language: 'language'
+    }; 
 
-module.exports = requestApi; 
+    request(this.options, function(error, response, body) {
+      if(error) { callback(error) }
+      if(!error && response.statusCode === 200) {
+        var result = JSON.parse(body);
+        var items = filterJSON(result.items, pickJSON);
+        updateDb(items, console.log);
+        callback(response); 
+      }
+    });
+}
 
+module.exports = LatestAPI; 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express =require('express'),
     mongoskin = require('mongoskin'),
     bodyParser = require('body-parser'),
-    getGithubApi = require('./getGithubApi')
+    LatestAPI = require('./getGithubApi')
     ;
 
 var app = express();
@@ -10,6 +10,9 @@ var app = express();
 app.use(bodyParser.json());
 
 var db = mongoskin.db('mongodb://@localhost:27017/repos', {safe:true});
+
+var port = 3000;
+var gh_url = 'https://api.github.com/'
 
 app.get('/', function(request, response) {
   response.send("Hello world");
@@ -22,5 +25,26 @@ app.get('/api/repos', function(request, response, next){
   }) 
 });
 
-app.listen(3000);
+app.listen(port, function() {
+  console.log("Successfully connect to port " + port);
+  var initAPI = new LatestAPI(gh_url);
+  initAPI.fetchAPI(function(response) {
+    if(response) {
+      console.log("Data initialization done.") 
+    }
+  });
+
+  var API = new LatestAPI(gh_url);
+  var interval = 3600*1000;
+  (function schedule() {
+    setTimeout(function() {
+      API.fetchAPI(function(response) {
+        if(response) 
+          schedule();
+      });
+    }, interval);
+  })();
+
+});
+
 
