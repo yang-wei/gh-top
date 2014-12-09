9a9de295539d839658fe59c30addcd48296eaf2b
yang-wei <yangweilim21@gmail.com>
2014-12-09 13:46:56 +0900

    draft for d3plus component

---
+++ components/treemap.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var React = require('react');
+var d3 = require('d3');
+var d3plus = require('d3plus');
+
+var TreeMap = React.createClass({
+  render: function() {
+     <p>This is {this.props.text}</p> 
+  }
+});
+
+exports.module = TreeMap;
