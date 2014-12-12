8fdd69a232ba31a55d85b0df32b7e9ea8e82478b
yang-wei <yangweilim21@gmail.com>
2014-12-12 13:18:40 +0900

    move dummy data to top dir

--- client.js
+++ client.js
@@ -3,7 +3,21 @@
 var React = require('react');
 var Hello = require('./components/hello');
 var Repo = require('./components/repo');
-var BarChart= require('./components/treemap');
+var Treemap = require('./components/treemap');
+// dummy
+var tree = {
+    name: "tree",
+    children: [
+        { name: "Word-wrapping comes for free in HTML", size: 16000 },
+        { name: "animate makes things fun", size: 8000 },
+        { name: "data data everywhere...", size: 5220 },
+        { name: "display something beautiful", size: 3623 },
+        { name: "flex your muscles", size: 984 },
+        { name: "physics is religion", size: 6410 },
+        { name: "query and you get the answer", size: 2124 }
+    ]
+};
+
 
 var App = React.createClass({
   render: function() {
@@ -13,7 +27,7 @@ var App = React.createClass({
           <link href='./bower_components/fontawesome/css/font-awesome.css' rel='stylesheet' />
         </head>
         <Hello text='React' />
-        <BarChart width={600} height={300} text='treemap'/>
+        <Treemap width={700} height={300} data={tree} value='size'/>
       </html>
     );
   }
