5f824ad0c23d96ec79da2ed6404ea63423bcc1b9
yang-wei <yangweilim21@gmail.com>
2014-12-20 00:27:34 +0900

    update client.js

--- client.js
+++ client.js
@@ -1,15 +1,33 @@
 'use strict';
 
 var React = require('react');
+var ReactAsync = require('react-async');
 var Treemap = require('./components/treemap');
+var superagent = require('superagent');
 
 var App = React.createClass({
-    getDefaultProps: function() {
-      return { 
-        data: [],
-        value: ''
-      }
+    getInitialState: function() {
+      return { repos: [] }
     },
+
+    componentWillMount: function() {
+      this.loadRepos();
+    },
+
+    componentDidMount: function() {
+      //this.loadRepos();
+    },
+
+    loadRepos: function() {
+      superagent
+        .get('http://localhost:5000/api/repos')
+        .set({'Access-Control-Allow-Origin': '*'})
+        .end(function(err, res) {
+          if(err) console.log(err);
+          this.setState({ repos: res.body }); 
+        }.bind(this));
+    },
+
     render: function() {
       return (
         <html>
@@ -19,11 +37,10 @@ var App = React.createClass({
           <body>
             <header>
               <h1>Github Repository in Treemap</h1>
+              <button onClick={this.loadRepos}>Load Data</button>
             </header>
             <div className='treemap-container'>
-              <Treemap width={960} height={500} 
-                      data={this.props.data} 
-                      value={this.props.value} />
+              <Treemap data={this.state.repos} value='stars' width={960} height={500} />
             </div>
             <script src='./public/bundle.js'></script>
           </body>
