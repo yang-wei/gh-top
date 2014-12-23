9cf1facb03f2fdff384a47a792229a2a038d3b4d
yang-wei <yangweilim21@gmail.com>
2014-12-23 17:14:11 +0900

    add languageBar component

---
+++ components/languageBar.js
@@ -0,0 +1,21 @@
+'use strict';
+var React = require('react');
+
+module.exports = React.createClass({
+  handleChange: function(e) {
+    this.props.changeLang(e.target.textContent);
+  },     
+  render: function() {
+    var languages = ['JavaScript', 'PHP', 'Ruby']; 
+    var options = languages.map(function(lang, i) { 
+      return (
+        <li key={i} lang={lang}><a href='#' >{lang}</a></li>
+      )
+    }.bind(this));
+    return (
+      <ul onClick={this.handleChange}>
+        {options}
+      </ul>
+    ) 
+  }
+});
