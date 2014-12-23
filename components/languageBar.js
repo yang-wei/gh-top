2af64ebbc64612757e70e11d0bc6ab025c817afe
yang-wei <yangweilim21@gmail.com>
2014-12-23 18:06:39 +0900

    add all to Array

--- components/languageBar.js
+++ components/languageBar.js
@@ -6,7 +6,7 @@ module.exports = React.createClass({
     this.props.changeLang(e.target.textContent);
   },     
   render: function() {
-    var languages = ['JavaScript', 'PHP', 'Ruby']; 
+    var languages = ['All', 'JavaScript', 'PHP', 'Ruby']; 
     var options = languages.map(function(lang, i) { 
       return (
         <li key={i} lang={lang}><a href='#' >{lang}</a></li>
