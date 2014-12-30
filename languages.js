1aa5f3d5387dacd58c3282554bbe3e473c20189d
yang-wei <yangweilim21@gmail.com>
2014-12-31 00:31:51 +0900

    add more information to language bar

--- languages.js
+++ languages.js
@@ -1,4 +1,22 @@
 'use strict';
 // top 15 on github.info on Dec 2014
-module.exports = ['All', 'JavaScript', 'Java', 'CSS', 'Python', 'Ruby', 'PHP', 'C++', 'C', 'Shell', 'C#', 'Objective-c', 'R', 'VimL', 'Go', 'Perl', 'Coffeescript'];
+module.exports = [ 	
+										{ search: 'all', label: 'All'}, 
+										{ search: 'javascript', label: 'JavaScript'}, 
+										{ search: 'java', label: 'Java'}, 
+										{ search: 'css', label: 'CSS'}, 
+										{ search: 'python', label: 'Python'}, 
+										{ search: 'ruby', label: 'Ruby'}, 
+										{ search: 'php', label: 'PHP'}, 
+										{ search: 'cpp', label: 'C++'}, 
+										{ search: 'c', label: 'C'}, 
+										{ search: 'shell', label: 'Shell'}, 
+										{ search: 'csharp', label: 'C#'}, 
+										{ search: 'objective-c', label: 'Objective-C'}, 
+										{ search: 'r', label: 'R'}, 
+										{ search: 'viml', label: 'VimL'}, 
+										{ search: 'go', label: 'Go'}, 
+										{ search: 'perl', label: 'Perl'}, 
+										{ search: 'coffeescript', label: 'CoffeeScript'} 
+								];
 
