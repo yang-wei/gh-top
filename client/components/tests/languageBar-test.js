9f1752760ad29850844ef2b22fd61e15f9bc2417
yang-wei <yangweilim21@gmail.com>
2015-02-01 11:00:31 +0900

    improve languageBar test

--- client/components/tests/languageBar-test.js
+++ client/components/tests/languageBar-test.js
@@ -3,22 +3,41 @@ var React = require('react/addons');
 var LanguageBarComp = require('../languageBar');
 var TestUtils = React.addons.TestUtils;
 var assert = require('assert');
+var sinon = require('sinon');
 
 describe('LanguageBar testing', function() {
 
   it('Should render language bar', function() {
-    var languages = ['JavaScript', 'PHP', 'CSS'];
+    var languages = [
+						{ search: 'all', label: 'All'}, 
+            { search: 'javascript', label: 'JavaScript'}, 
+            { search: 'java', label: 'Java'}, 
+            { search: 'css', label: 'CSS'}
+    ]; 
+
     var LanguageBarFactory = React.createFactory(LanguageBarComp); 
     var LanguageBar = LanguageBarFactory({
-      lang: languages 
+      languages: languages,
+      changeLang: sinon.spy() 
     });
     var Body = document.getElementsByTagName('body')[0];
 
     // Now render it
     var renderedComponent = React.render(LanguageBar, Body); 
 
+    var ul = TestUtils.scryRenderedDOMComponentsWithTag(renderedComponent, 'ul');
+    assert.equal(ul.length, 1);
+
     var li = TestUtils.scryRenderedDOMComponentsWithTag(renderedComponent, 'li');
-    assert(li.length, languages.length);
-  })
+    assert.equal(li.length, languages.length);
+
+    var links = TestUtils.scryRenderedDOMComponentsWithTag(renderedComponent, 'a');
+    assert.equal(links.length, languages.length);
+    
+    assert(!renderedComponent.props.changeLang.called);
+    TestUtils.Simulate.click(links[1]);
+    assert(renderedComponent.props.changeLang.called)
+
+  });
 
 })
